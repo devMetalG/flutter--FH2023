3a16b9f3d512c5dd55e9ba8f23e0c3d7a0f035fe
MetalG <fra_borjon@hotmail.com>
2023-04-10 18:21:23 -0600

    First commit & hello world

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,12 @@
+void main(){
+  
+//   var  myName = 'Memo';
+  String  myName = 'Memo';
+  
+  final name1 = 'Memo';
+  late final name2 = 'Memo'; 
+  
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  
+}
